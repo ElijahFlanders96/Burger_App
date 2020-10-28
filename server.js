66a1fb8f8b6f6ecc4f11e4e8511081420e2c2077
Elijah Flanders <elijahflanders15@gmail.com>
2020-10-28 18:11:14 -0500

    set up boilerplate for server.js file

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const connection = require('./config/connection');
+const routes = require('./controllers/burger_controller')
+
+const app = express(); 
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+
+app.engine('handlebars', exphbs({ defaultLayout: "main" }));
+app.set('view engine', "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log('Server listening on http://localhost:' + PORT);
+})
