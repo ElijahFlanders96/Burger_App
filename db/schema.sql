aa1db2286653dcd0ebb293c21c4678417f8e5ff6
Elijah Flanders <elijahflanders15@gmail.com>
2020-10-28 17:48:26 -0500

    set up the database in mysql workbench and set up the connection in my connection.js file

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+CREATE TABLE burgers(
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(35) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
+  PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES 
+  ('Breakfast Burger', 0),
+  ('The Crazy E Burger', 0),
+  ('Cowgirl Burger', 0),
+  ('Thanksgiving Burger', 0);
