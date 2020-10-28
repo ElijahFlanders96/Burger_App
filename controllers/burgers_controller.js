ff10cbca20e7fa197f85515a1e9ea4ab02768fdf
Elijah Flanders <elijahflanders15@gmail.com>
2020-10-28 18:20:42 -0500

    wrote boilerplate routes for the controller folder

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const burger = require("../models/burger");
+
+const app = express();
+
+app.get("/", function(req, res) {
+
+});
+
+app.post("/api/burgers", function(req, res) {
+
+});
+
+app.put("/api/burgers/:id", function(req, res) {
+
+});
